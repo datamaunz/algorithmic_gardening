fa6712757ab618a6f177de4e9ec81b3aac70c3bf
datamaunz <datamaunz@gmail.com>
2022-10-21 11:28:12 -0400

    minor changes

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import streamlit as st
 def main():
         
     st.set_page_config(
-    layout="wide",
-    page_title="Algorithmic Gardening",
-    page_icon = "🪴")
+        layout="wide",
+        page_title="Algorithmic Gardening",
+        page_icon = "🪴")
     
     st.markdown("## 🪴 Algorithmic Gardening")
+    
+    text = st.sidebar.text_input("Write Something")
+    
+    st.markdown(text)
     
     
     
