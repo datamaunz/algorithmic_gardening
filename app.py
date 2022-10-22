ef09f821fba5fd92dab66170c7450d6867f2c7bb
datamaunz <datamaunz@gmail.com>
2022-10-21 21:10:09 -0400

    network graphs added closes #13

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from src.load_data import *
 from src.plant_distribution_functions import buckets_generator
 from src.helper_functions import *
 
+ 
+
+
 def main():
         
     st.set_page_config(
@@ -11,25 +14,23 @@ def main():
         page_icon = "🌾")
     
     st.markdown("## 🪴 Algorithmic Gardening")
-    st.markdown("---")
+    
     
     
     df = load_plant_matrix_data("data/outputs/09162022_192558.csv")
     plant_names = df.index
     
-    col1, col2 = st.columns([0.8, 0.2])
-    selected_plants = col1.multiselect("Select plants for ideal pairing", plant_names)
-    n_columns = col2.number_input("Select maximum number of columns per row", value=8)
+    #col1, col2 = st.columns([0.8, 0.2])
+    selected_plants = st.sidebar.multiselect("Select plants for ideal pairing", plant_names)
+    n_columns = st.sidebar.number_input("Select maximum number of columns per row", value=4)
     
     
     filtered_df = filter_df_by_index(df, selected_plants)
     bucket_dict = buckets_generator(filtered_df)
     
+    display_network_graph_per_bucket(filtered_df, bucket_dict, n_columns)
     
     
-            
-        
-    display_plants_per_bucket(bucket_dict, n_columns)
     
     
         
